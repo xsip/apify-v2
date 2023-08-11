ae72049b5cc171d83f2e4aafa67f5b0ee011cf04
Kerschbaumer Stefan <xsip@pm.me>
2023-08-11 17:39:01 +0200

    fixes imports

--- src/apify/amazon/amazon.apified.service.ts
+++ src/apify/amazon/amazon.apified.service.ts
@@ -2,9 +2,7 @@ import { AmazonApifyModel } from './model'
 import { Apify, ApifyServiceOptions } from '../decorator'
 import * as fs from 'fs'
 import { Page } from 'puppeteer'
-import * as console from 'console'
-import * as console from 'console'
-import * as fs from 'fs'
+
 @Apify<AmazonApifyModel>({
   elementContainerSelector: '[data-component-type="s-search-result"]',
   childSelectors: {
